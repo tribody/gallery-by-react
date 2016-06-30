6a0ca3af4212b9d9ea855c2dae27210bab6dae3d
River He <hctongxue@163.com>
2016-06-30 17:04:17 +0800

    a little modify

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,60 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
-class AppComponent extends React.Component {
+//获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function () {
+  for (let i=0, j=imageDatasArr.length; i<j; i++) {
+    var singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+    imageDatasArr[i] = singleImageData;
+  }
+
+  return imageDatasArr;
+})(imageDatas);
+
+class ImgFigure extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <span>hello</span>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}
+             alt={this.props.data.title}
+        />
+        <figcaption>
+          <h2>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+class AppComponent extends React.Component {
+  render() {
+
+    var controllerUnits = [],
+        imgFigures = [];
+
+        imageDatas.forEach(function (value) {
+          imageFigures.push(<ImgFigure data={value} />);
+        });
+
+    return (
+      <section className="stage">
+        <section className="img-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+          {imgFigures}
+        </nav>
+      </section>
     );
   }
 }
